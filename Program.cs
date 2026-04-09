8fe92971a6b9f5e42a2c9961cf630a98d60a0301
sghita235 <s.ghita@students.amikom.ac.id>
2026-04-09 08:32:15 +0700

    adding vars and methods

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 25.61.0291 Shakila Ghita
+
+int a = 10, b = 11
+
+static internal Penjumlahan(int a, int b)
+{
+    return a+b;
+}
+
+static int Pengurangan(int a, int b)
+{
+    return a-b;
+}
+
+static int Perkalian(int a, int b)
+{
+    return a*b;
+} 
+
+static int Pembagian (int a, int b)
+{
+    return a/b
+}
+
+Console.WriteLine(a,"+" b, "=" +Penjumlahan(a,b));
+Console.WriteLine(a,"-" b, "=" +Pengurangan(a,b));
+Console.WriteLine(a,"*" b, "=" +Perkalian(a,b));
+Console.WriteLine(a,"/" b, "=" +Pembagian(a,b));
+
